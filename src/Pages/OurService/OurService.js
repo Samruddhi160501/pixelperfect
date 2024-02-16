7aa2af066c4cf5e98640b2bc70b8888b70df0672
Samruddhi16 <gorghatesamruddhi@gmail.com>
2024-02-16 10:15:16 +0530

    logo added and name of app changed

--- src/Pages/OurService/OurService.js
+++ src/Pages/OurService/OurService.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Header from "../../Layouts/Header/Header";
 import classes from "./OurService.module.css";
 import Container2 from "../../Layouts/Container2/Container2";
-import Footer from "../../components/Footer/Footer";
+import Footer from "../../Layouts/Footer/Footer";
 import Container4 from "../../Layouts/Container4/Container4";
 import Container3 from "../../Layouts/Container3/Container3";
 import Highlights from "../../Layouts/Highlights/Highlights";
