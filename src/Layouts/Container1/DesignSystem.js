d3f72d70005781cea52c5574078eff6c6c4f04e7
Samruddhi16 <gorghatesamruddhi@gmail.com>
2024-02-16 13:15:03 +0530

    overflow removed

---
+++ src/Layouts/Container1/DesignSystem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "../../Pages/OurService/OurService.module.css";
+function DesignSystem() {
+  return (
+    <div className={classes.heading}>
+      <h1 className={classes.text}>Design Systems for scale</h1>
+      <p className={classes.para}>
+        Add consistency to the scaling up of User Experience with an organized
+        assembly of guidelines, components and documentation.
+      </p>
+    </div>
+  );
+}
+
+export default DesignSystem;
---
+++ src/Layouts/FloatContainer/FloatContainer.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import classes from "../../Pages/OurService/OurService.module.css";
+import { designmenu } from "../../utils/data";
+
+function FloatContainer() {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 1024);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsSmallScreen(window.innerWidth <= 1024);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <div
+      className={classes.designSystem}
+      style={{ position: "relative", zIndex: 2 }}
+    >
+      <h3 className={classes.heading2}>
+        {isSmallScreen ? "This is ideal for" : "Design Systems are ideal for"}
+      </h3>
+      <hr className={classes.line} />
+      <div className={classes.designContainer}>
+        {designmenu.map((item) => (
+          <div
+            key={item.id}
+            className={
+              item.id === "/02" ? classes.cardHighlighted : classes.card
+            }
+          >
+            <p>{item.id}</p>
+            <p >{item.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FloatContainer;
